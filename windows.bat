5df35c58bad493196848218a146132b84c52458f
Mike Hodgson <mikehhodgson@users.noreply.github.com>
2018-09-08 11:08:43 +0800

    windows symlinks

--- windows.bat
+++ windows.bat
@@ -1,2 +1,7 @@
 mkdir %userprofile%\Documents\WindowsPowerShell
-echo . $env:appdata\dotfiles\profile.ps1 >> %userprofile%\Documents\WindowsPowerShell\profile.ps1
+:: echo . %appdata%\dotfiles\profile.ps1 >> %userprofile%\Documents\WindowsPowerShell\profile.ps1
+mklink %userprofile%\Documents\WindowsPowerShell\profile.ps1 %cd%\profile.ps1
+
+mklink %userprofile%\.gitconfig %cd%\gitconfig
+
+mklink %appdata%\.emacs %cd%\emacs
