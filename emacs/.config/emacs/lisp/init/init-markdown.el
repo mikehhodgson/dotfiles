bf39fc716f6cf4f99aa256ee729ed5a24d6c2889
Mike Hodgson <mikehhodgson@users.noreply.github.com>
2026-05-26 19:50:57 +0800

    refactor markdown init

--- emacs/.config/emacs/lisp/init/init-markdown.el
+++ emacs/.config/emacs/lisp/init/init-markdown.el
@@ -5,23 +5,23 @@
   :init
   (setq markdown-hide-markup nil
         markdown-gfm-uppercase-checkbox t
-        ;; markdown-mode code block syntax highlighting  
-        markdown-fontify-code-blocks-natively t))
-
-(setq markdown-header-scaling t)
-
-(add-hook 'markdown-mode-hook #'variable-pitch-mode)
-(add-hook 'markdown-mode-hook #'visual-fill-column-mode)
-
-;; Keep code/table regions fixed-pitch
-(setq-local face-remapping-alist
-            '((markdown-code-face fixed-pitch)
-              (markdown-inline-code-face fixed-pitch)
-              (markdown-pre-face fixed-pitch)
-              (markdown-table-face fixed-pitch)
-              (markdown-language-keyword-face fixed-pitch)))
-
-(with-eval-after-load 'markdown-mode
+        markdown-header-scaling t
+        ;; markdown-mode code block syntax highlighting
+        markdown-fontify-code-blocks-natively t)
+  :hook ((markdown-mode . variable-pitch-mode)
+         (markdown-mode . visual-fill-column-mode)
+         (markdown-mode . (lambda ()
+                            "Keep Markdown code and table regions fixed-pitch."
+                            (dolist (face '(markdown-code-face
+                                            markdown-inline-code-face
+                                            markdown-pre-face
+                                            markdown-table-face
+                                            markdown-language-keyword-face))
+                              (face-remap-add-relative face 'fixed-pitch))))
+         (markdown-mode . (lambda ()
+                            (keymap-set markdown-mode-map "M-n" #'scroll-up-line)
+                            (keymap-set markdown-mode-map "M-p" #'scroll-down-line))))
+  :config
   (set-face-attribute 'markdown-header-face-1 nil :height 1.8 :family "Nasalization")
   (set-face-attribute 'markdown-header-face-2 nil :height 1.6 :family "Nasalization")
   (set-face-attribute 'markdown-header-face-3 nil :height 1.4 :family "Nasalization")
@@ -33,10 +33,5 @@
   (set-face-attribute 'markdown-inline-code-face nil :inherit 'fixed-pitch)
   (set-face-attribute 'markdown-pre-face         nil :inherit 'fixed-pitch)
   (set-face-attribute 'markdown-table-face       nil :inherit 'fixed-pitch))
-
-(add-hook 'markdown-mode-hook
-          (lambda ()
-	          (keymap-set markdown-mode-map "M-n" 'scroll-up-line)
-	          (keymap-set markdown-mode-map "M-p" 'scroll-down-line)))
 
 (provide 'init-markdown)
