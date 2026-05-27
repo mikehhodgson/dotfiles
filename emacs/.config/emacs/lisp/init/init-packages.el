ba3c73dc199bd0d918ca5c72d051128acd4d95fc
Mike Hodgson <mikehhodgson@users.noreply.github.com>
2026-05-27 19:02:48 +0800

    hide byte compiler warnings

--- emacs/.config/emacs/lisp/init/init-packages.el
+++ emacs/.config/emacs/lisp/init/init-packages.el
@@ -10,4 +10,12 @@
 
 (setq use-package-always-ensure t)
 
+;; Emacs will show a buffer that contains any byte compiler warnings
+;; from installing a new package. This will prevent those from
+;; showing. They are still available in the buffer list.
+(add-to-list 'display-buffer-alist
+             '("\\`\\*\\(Warnings\\|Compile-Log\\)\\*\\'"
+               (display-buffer-no-window)
+               (allow-no-window . t)))
+
 (provide 'init-packages)
