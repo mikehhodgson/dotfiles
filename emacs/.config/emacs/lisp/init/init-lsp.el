ad31d473cd17ca6a383c23cbeceeec03f4ac7252
Mike Hodgson <mikehhodgson@users.noreply.github.com>
2026-06-06 21:06:40 +0800

    python eglot, python-lsp-server

--- emacs/.config/emacs/lisp/init/init-lsp.el
+++ emacs/.config/emacs/lisp/init/init-lsp.el
@@ -6,9 +6,11 @@
   :hook ((c++-mode . eglot-ensure)
          (c-mode . eglot-ensure)
          (js-ts-mode . eglot-ensure)
+         (python-mode . eglot-ensure)
          (tsx-ts-mode . eglot-ensure))
   :config
   (add-to-list 'eglot-server-programs
-               '((c++-mode c-mode) . ("clangd"))))
+               '((c++-mode c-mode) . ("clangd"))
+               '((python-mode) . ("pylsp")))) ; sudo pacman -S python-lsp-server
 
 (provide 'init-lsp)
